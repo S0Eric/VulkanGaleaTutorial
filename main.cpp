baeecde3a1df3921b09c22ebeb4d61edaa30e7aa
Eric Schmucker <eric.schmucker@blazemetrics.com>
2021-07-23 09:01:40 -0600

    Video 5.1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+#include "first_app.h"
+
+int main() {
+	lve::FirstApp app{};
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
